b08f3affe2ab5385e2ff1523c405d7f24f681cf6
duridudu <acd1026@naver.com>
2024-08-18 17:50:50 +0900

    test

---
+++ QrCode_tutorial/Pods/Target Support Files/Pods-QrCode_tutorial-QrCode_tutorialUITests/Pods-QrCode_tutorial-QrCode_tutorialUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_QrCode_tutorial_QrCode_tutorialUITests : NSObject
+@end
+@implementation PodsDummy_Pods_QrCode_tutorial_QrCode_tutorialUITests
+@end
